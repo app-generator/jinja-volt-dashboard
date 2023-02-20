018883cc45ab81809a830b2ca4a8ddbd0708b8af
imran-helios <imran.mridha@heliosinsider.com>
2023-02-20 17:25:28 +0600

    Render pages

--- apps/views.py
+++ apps/views.py
@@ -12,30 +12,92 @@ from apps import app
 
 # App main route + generic routing
 @app.route('/', defaults={'path': 'index.html'})
-@app.route('/<path>')
-def index(path):
+@app.route('/')
+def index():
+  try:
+    return render_template( 'pages/index.html', segment='index', parent='pages')
+  except TemplateNotFound:
+    return render_template('pages/index.html'), 404
 
-    try:
+# Pages -- Dashboard
+@app.route('/pages/dashboard/')
+def pages_dashboard():
+  return render_template('pages/dashboard/dashboard.html', segment='dashboard', parent='pages')
 
-        # Detect the current page
-        segment = get_segment( request )
+# Pages
 
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template( 'home/' + path, segment=segment )
-    
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
+@app.route('/pages/transactions/')
+def pages_transactions():
+  return render_template('pages/transactions.html', segment='transactions', parent='pages')
 
-def get_segment( request ): 
+@app.route('/pages/settings/')
+def pages_settings():
+  return render_template('pages/settings.html', segment='settings', parent='pages')
 
-    try:
+@app.route('/pages/upgrade-to-pro/')
+def pages_upgrade_to_pro():
+  return render_template('pages/upgrade-to-pro.html', segment='upgrade_to_pro', parent='pages')
 
-        segment = request.path.split('/')[-1]
+# Pages -- Tables
 
-        if segment == '':
-            segment = 'index'
+@app.route('/pages/tables/bootstrap-tables/')
+def pages_tables_bootstrap_tables():
+  return render_template('pages/tables/bootstrap-tables.html', segment='bootstrap_tables', parent='tables')
 
-        return segment    
+# Pages -- Pages examples
 
-    except:
-        return None  
+@app.route('/pages/examples/404/')
+def pages_examples_404():
+  return render_template('pages/examples/404.html', segment='404', parent='pages')
+
+@app.route('/pages/examples/500/')
+def pages_examples_500():
+  return render_template('pages/examples/500.html', segment='500', parent='pages')
+
+# Accounts
+
+@app.route('/accounts/sign-in/')
+def accounts_sign_in():
+  return render_template('accounts/sign-in.html', segment='sign_in', parent='accounts')
+
+@app.route('/accounts/sign-up/')
+def accounts_sign_up():
+  return render_template('accounts/sign-up.html', segment='sign_up', parent='accounts')
+
+@app.route('/accounts/forgot-password/')
+def accounts_forgot_password():
+  return render_template('accounts/forgot-password.html', segment='forgot_password', parent='accounts')
+
+@app.route('/accounts/reset-password/')
+def accounts_reset_password():
+  return render_template('accounts/reset-password.html', segment='reset_password', parent='accounts')
+
+@app.route('/accounts/lock/')
+def accounts_lock():
+  return render_template('accounts/lock.html', segment='lock', parent='accounts')
+
+@app.route('/accounts/password-change/')
+def accounts_password_change():
+  return render_template('accounts/password-change.html', segment='password-change', parent='accounts')
+
+# Pages Components
+
+@app.route('/pages/components/buttons/')
+def pages_components_buttons():
+  return render_template('pages/components/buttons.html', segment='buttons', parent='components')
+
+@app.route('/pages/components/notifications/')
+def pages_components_notifications():
+  return render_template('pages/components/notifications.html', segment='notifications', parent='components')
+
+@app.route('/pages/components/forms/')
+def pages_components_forms():
+  return render_template('pages/components/forms.html', segment='forms', parent='components')
+
+@app.route('/pages/components/modals/')
+def pages_components_modals():
+  return render_template('pages/components/modals.html', segment='modals', parent='components')
+
+@app.route('/pages/components/typography/')
+def pages_components_typography():
+  return render_template('pages/components/typography.html', segment='typography', parent='components')
